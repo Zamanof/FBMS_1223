6aae960d9a029d72f9ece109a7ccfa724008c5c9
zamanof <nadir.r.zamanov@gmail.com>
2023-05-28 09:12:38 +0400

    Ado lesson 2

--- Paramtrized/Program.cs
+++ Paramtrized/Program.cs
@@ -11,8 +11,16 @@ builder.SetBasePath(Directory.GetCurrentDirectory());
 builder.AddJsonFile("AppConfig.json");
 var config = builder.Build();
 connecionString = config.GetConnectionString("DefaultConnection")!;
+conn = new SqlConnection(connecionString);
 
-string authorName = "";
+string authorName = "Boris";
 SqlParameter parameter = new SqlParameter();
 parameter.ParameterName = "@authorname";
 parameter.SqlDbType = SqlDbType.NVarChar;
+parameter.Value = authorName;
+
+conn.Open();
+SqlCommand cmd = new SqlCommand("SELECT @authorname FROM Authors;");
+//cmd.Parameters.Add(parameter);
+//cmd.Parameters.Add("@authorname", SqlDbType.NVarChar).Value = authorName;
+cmd.Parameters.AddWithValue("@authorname", authorName);
