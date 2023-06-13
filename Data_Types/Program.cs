21b85e448617d5307e92fff48dcaa2b2e5a2f867
zamanof <nadir.r.zamanov@gmail.com>
2023-06-13 11:57:31 +0400

    Fluent API

---
+++ Data_Types/Program.cs
@@ -0,0 +1,69 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using(var db = new TestContext())
+{
+    var test = new Test();
+    db.Tests.Add(test);
+    db.SaveChanges();
+}
+
+#region SQL <--> C# Data types
+/*
+MS SQL          C#
+
+int             int (Int32)
+bit             bool (Boolean)
+smallint        short (Int16)
+tinyint         byte (Int8)
+bigint          long (Int64)
+
+
+date            DateTime
+datetime        DateTime
+datetime2       DateTime
+
+float           double (Double)
+real            float (Single)
+
+decimal         decimal
+money           decimal
+
+char            string
+nchar           string
+varchar         string
+nvarchar        string
+text            string
+ntext           string
+
+ 
+ */
+#endregion
+
+class Test
+{
+    public int Id { get; set; }
+    public decimal field2 { get; set; }
+    public bool field3 { get; set; }
+    public short field4 { get; set; }
+    public short field5 { get; set; }
+    public long field1 { get; set; }
+    public float field6 { get; set; }
+    public double field7 { get; set; }
+
+}
+
+class TestContext : DbContext
+{
+    public DbSet<Test> Tests { get; set; }
+
+    public TestContext()
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=TestDatatype;Integrated Security=True;");
+    }
+}
---
+++ FluentAPI_DataAnnotation/Program.cs
@@ -0,0 +1,90 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Cryptography.X509Certificates;
+
+// convention
+// Data Annotation
+// Fluent API
+
+using (var db = new ProductContext())
+{
+    var country = new Country() { Name = "Azerbaijan" };
+    var manufacturer = new Manufacturer() { Name = "Milla", Country = country };
+    var product = new Product() { Name = "Ayran", Manufacturer = manufacturer, Price = 1 };
+    db.Products.Add(product);
+    db.SaveChanges();
+
+}
+
+ //[Index(nameof(Name), IsUnique =true)]
+public class Product
+{
+    //[Key]
+    public int ProductNumber { get; set; }
+    [Required]
+    [MaxLength(20)]
+    public string? Name { get; set; }
+    [Column("Product_Price")]
+    public float Price { get; set; }
+    [NotMapped]
+    public string Description { get; set; }
+    public int ManufacturerId { get; set; }
+
+    public Manufacturer? Manufacturer { get; set; }
+
+}
+
+//[NotMapped]
+public class Country
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string? Name { get; set; }
+}
+
+public class Manufacturer
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public Product Product { get; set; }
+    
+    public int CountryId { get; set; }
+    public Country? Country { get; set; }
+}
+
+public class ProductContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    public ProductContext()
+    {
+        Database.EnsureDeleted();
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=FluentApiProducts;Integrated Security=True;TrustServerCertificate=True;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Manufacturer>().Property(m => m.Name).IsRequired();
+        modelBuilder.Ignore<Country>();
+        modelBuilder.Entity<Product>(
+            entity =>
+            {
+                entity.HasKey(p => p.ProductNumber);
+                //entity.HasIndex(p => p.Name).IsUnique();
+                //entity.Property("Price").HasColumnName("Product_price");
+            }
+     );
+        modelBuilder.Entity<Product>()
+            .HasOne(p => p.Manufacturer)
+            .WithOne(m => m.Product);
+    }
+}
+
