16a03eb5a0701365684a44e9b0a50d5126250d4e
Nadir Zamanov <Zamanov@itstep.edu.az>
2023-07-20 09:31:00 +0400

    Networ lesson 1

---
+++ Socket client/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+var ipAdress = IPAddress.Parse("127.0.0.1");
+var port = 27001;
+
+Socket listener = new Socket(
+    AddressFamily.InterNetwork,
+    SocketType.Stream,
+    ProtocolType.Tcp
+    );
+
+var endPoint = new IPEndPoint(ipAdress, port);
+listener.Bind(endPoint);
+var backlog = 1;
+
+Console.WriteLine("Listener listen...");
+listener.Listen(backlog);
+
+var length = 0;
+
+var bytes = new byte[1024];
+var message = string.Empty;
+
+Socket 
+
